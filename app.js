16e7af16f5f36e5e6966b967b73ecc924de104de
gvivan <103859388+gvivan@users.noreply.github.com>
2022-12-27 01:57:25 -0500

    add(): functionality to add tasks to the to-do list

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = [];
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var currDay = today.getDay();
-  var day = "";
 
-  if (currDay === 0) {
-    day = "Sunday";
-  } else if (currDay === 1) {
-    day = "Monday";
-  } else if (currDay === 2) {
-    day = "Tuesday";
-  } else if (currDay === 3) {
-    day = "Wednesday";
-  } else if (currDay === 4) {
-    day = "Thursday";
-  } else if (currDay === 5) {
-    day = "Friday";
-  } else {
-    day = "Saturday";
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    day: day
+    day: day,
+    items: items
   });
-});
+})
+
+app.post("/", function(req, res) {
+  items.push(req.body.newItem);
+
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("server started on port 3000");
