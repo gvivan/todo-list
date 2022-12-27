74546464a9f691b4c0c2a9ae8a803a127c038c61
gvivan <103859388+gvivan@users.noreply.github.com>
2022-12-27 00:58:02 -0500

    add ejs to the project

--- app.js
+++ app.js
@@ -5,10 +5,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+  var today = new Date();
+  var currDay = today.getDay();
+  var day = "";
+
+  if (currDay === 0) {
+    day = "Sunday";
+  } else if (currDay === 1) {
+    day = "Monday";
+  } else if (currDay === 2) {
+    day = "Tuesday";
+  } else if (currDay === 3) {
+    day = "Wednesday";
+  } else if (currDay === 4) {
+    day = "Thursday";
+  } else if (currDay === 5) {
+    day = "Friday";
+  } else {
+    day = "Saturday";
+  }
+
+  res.render("list", {
+    day: day
+  });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server started on port 3000");
 })
