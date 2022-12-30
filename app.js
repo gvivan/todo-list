a4afde92c30ea23e5502876420ca445a7fd716cc
gvivan <103859388+gvivan@users.noreply.github.com>
2022-12-30 14:29:16 -0500

    add(): work list and about page

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = ["ayo", "say"];
+var items = [];
+var workItems = [];
 
 app.get("/", function(req, res) {
   var today = new Date();
@@ -29,13 +30,25 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-  items.push(req.body.newItem);
 
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
 })
 
-app.get("/work", function() {
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    items: workItems
+  });
+})
 
+app.get("/about", function(req, res) {
+  res.render("about");
 })
 
 app.listen(3000, function() {
